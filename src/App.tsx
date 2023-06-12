d12b05039c90413465b989e8ade5a3eef0dd688b
Garaev Matvei <Matthias78@yandex.ru>
2023-06-12 21:53:51 +0300

    l

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,31 @@
-import React from 'react';
 import { Product } from './components/Product';
-import {products} from './data/products';
+import { useProducts } from './hooks/products';
+import { Loader } from './components/Loader';
+import { ErrorMessage } from './components/ErrorMessage';
+import { Modal } from './components/Modal';
+import { CreateProduct } from './components/CreateProduct';
+import { useState } from 'react';
+
+
+
 function App() {
+const {loading,error,products,addProduct} = useProducts()
+const [modal,setModal] = useState(true)
+const createHandler = (product: IProduct) => {
+	setModal(false)
+	addProduct(product)
+}
+
 return (
 <div className='container mx-auto max-w-2xl pt-5'>
-<Product product={products[0]} />
-<Product product={products[1]} />
+{loading && <Loader />}
+{error  && <ErrorMessage error={error}/>}
+{products.map(product => <Product product={product} key={product.id} />)}
+{modal && <Modal title="Create new product">
+<CreateProduct onCreate={createHandler}/>
+</Modal>}
 </div>
-)
-   
+)   
 }
 
 export default App;
