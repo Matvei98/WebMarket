e9746baa8ce0a5a1c70f28d4092fe8733bff3066
Garaev Matvei <Matthias78@yandex.ru>
2023-06-14 17:29:33 +0300

    добавил навигацию, сделал кнопку для модального окна

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,18 @@
-import { Product } from './components/Product';
-import { useProducts } from './hooks/products';
-import { Loader } from './components/Loader';
-import { ErrorMessage } from './components/ErrorMessage';
-import { Modal } from './components/Modal';
-import { CreateProduct } from './components/CreateProduct';
-import { useState } from 'react';
-
-
+import {Route, Routes} from 'react-router-dom'
+import {ProductsPage} from './pages/ProductsPage'
+import {AboutPage} from './pages/AboutPage'
+import {Navigation} from './components/Navigation'
 
 function App() {
-const {loading,error,products,addProduct} = useProducts()
-const [modal,setModal] = useState(true)
-const createHandler = (product: IProduct) => {
-	setModal(false)
-	addProduct(product)
-}
-
-return (
-<div className='container mx-auto max-w-2xl pt-5'>
-{loading && <Loader />}
-{error  && <ErrorMessage error={error}/>}
-{products.map(product => <Product product={product} key={product.id} />)}
-{modal && <Modal title="Create new product">
-<CreateProduct onCreate={createHandler}/>
-</Modal>}
-</div>
-)   
+  return (
+    <>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={ <ProductsPage /> } />
+        <Route path="/about" element={ <AboutPage /> } />
+      </Routes>
+    </>
+  )
 }
 
 export default App;
